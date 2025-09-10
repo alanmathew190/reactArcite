40ed8e3e5ba227d81a78833108865f58ac3f64f3
Alan <alanmathew190@gmail.com>
2025-09-10 13:40:54 +0530

    push day4

---
+++ day4-2/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  let incrementCount = () => {
+  setCount(count+1)
+  }
+
+  useEffect(() => {
+    document.title=`hai ${count}`
+  })
+    
+  
+  return (
+    <>
+      
+      <h1>COUNTER</h1>
+        <h1>{count}</h1>
+        <button onClick={incrementCount}>+</button>
+        
+    </>
+  )
+}
+
+export default App
