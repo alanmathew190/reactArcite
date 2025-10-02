737ff9fc4129763f1e86320fb20ad03d98993ad2
Alan <alanmathew190@gmail.com>
2025-10-02 17:26:09 +0530

    push day9

---
+++ day9/day9-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <button>+</button>
+      <p>0</p>
+      <button>-</button>
+    </div>
+  )
+}
+
+export default App
