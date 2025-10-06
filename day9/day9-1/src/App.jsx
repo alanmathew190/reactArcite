07705ce4404e986bca8a6911c7be28415ff6c659
Alan <alanmathew190@gmail.com>
2025-10-06 19:15:16 +0530

    push day9

--- day9/day9-1/src/App.jsx
+++ day9/day9-1/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
+import { decrement, increment } from './components/redux/CounterSlice';
+import { useDispatch, useSelector } from 'react-redux' 
 
 function App() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
   return (
     <div>
       <h1>Counter App</h1>
-      <button>+</button>
-      <p>0</p>
-      <button>-</button>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <p>{count }</p>
+      <button onClick={()=>dispatch(decrement())}>-</button>
     </div>
   )
 }
